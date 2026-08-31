7ac505d328ff6fb210547b7389b4292ac61d4a3d
nbuzzerio <nick.buzzerio@hotmail.com>
2026-08-30 21:34:58 -0400

    Extend wolf quest into raider threat

--- Assets/Scripts/Dialogue/NPCDialogue.cs
+++ Assets/Scripts/Dialogue/NPCDialogue.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class NPCDialogue : MonoBehaviour
 {
     [SerializeField] private WolfQuest wolfQuest;
+    [SerializeField] private TextMeshProUGUI dialogueText;
     public GameObject talkPromptUI;
     public GameObject dialogueUI;
 
@@ -18,12 +20,49 @@ public class NPCDialogue : MonoBehaviour
         }
 
         dialogueOpen = !dialogueOpen;
-        if (dialogueOpen && wolfQuest != null)
+
+        if (dialogueOpen)
         {
-            wolfQuest.StartQuest();
+            UpdateDialogue();
         }
+
         dialogueUI.SetActive(dialogueOpen);
         talkPromptUI.SetActive(!dialogueOpen);
+    }
+
+    private void UpdateDialogue()
+    {
+        if (wolfQuest == null || dialogueText == null)
+        {
+            return;
+        }
+
+        switch (wolfQuest.CurrentStage)
+        {
+            case WolfQuest.QuestStage.NotStarted:
+                dialogueText.text =
+                    "Be careful... I've heard wolves have been spotted beyond the trees.";
+                wolfQuest.StartQuest();
+                break;
+
+            case WolfQuest.QuestStage.WolfActive:
+                dialogueText.text =
+                    "The wolf is still out there beyond the trees. Please be careful.";
+                break;
+
+            case WolfQuest.QuestStage.WolfDefeated:
+                dialogueText.text =
+                    "Thank you. That wolf had been troubling us for days. " +
+                    "But we have a larger problem now. Raiders have taken over an old fort nearby " +
+                    "and have been terrorizing the village.";
+                wolfQuest.IntroduceRaiderThreat();
+                break;
+
+            case WolfQuest.QuestStage.RaiderThreatIntroduced:
+                dialogueText.text =
+                    "The raiders are still holding the old fort nearby. The village needs help.";
+                break;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/Quest/WolfQuest.cs
+++ Assets/Scripts/Quest/WolfQuest.cs
@@ -2,18 +2,48 @@ using UnityEngine;
 
 public class WolfQuest : MonoBehaviour
 {
+    public enum QuestStage
+    {
+        NotStarted,
+        WolfActive,
+        WolfDefeated,
+        RaiderThreatIntroduced
+    }
+
     [SerializeField] private GameObject wolfEnemy;
 
-    private bool questStarted;
+    private QuestStage currentStage = QuestStage.NotStarted;
+
+    public QuestStage CurrentStage => currentStage;
 
     public void StartQuest()
     {
-        if (questStarted)
+        if (currentStage != QuestStage.NotStarted)
         {
             return;
         }
 
-        questStarted = true;
+        currentStage = QuestStage.WolfActive;
         wolfEnemy.SetActive(true);
+    }
+
+    public void CompleteWolfEncounter()
+    {
+        if (currentStage != QuestStage.WolfActive)
+        {
+            return;
+        }
+
+        currentStage = QuestStage.WolfDefeated;
+    }
+
+    public void IntroduceRaiderThreat()
+    {
+        if (currentStage != QuestStage.WolfDefeated)
+        {
+            return;
+        }
+
+        currentStage = QuestStage.RaiderThreatIntroduced;
     }
 }
